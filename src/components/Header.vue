ec37520eca6ad49d32e7a3d84a5d4085d7eb447d
2401849 <2401849@estudante.uab.pt>
2024-11-05 20:15:58 +0000

    added responsive behavior mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
       <a href="https://portal.uab.pt/" target="_blank">
         <img src="@/assets/pic1.png" alt="Picture 1" class="header-image" />
       </a>
-      <h1>Pedro Morais' Curriculum Vitae</h1>
+      <h1>Pedro Morais's Curriculum Vitae</h1>
       <a href="https://www.utad.pt/" target="_blank">
         <img src="@/assets/pic2.png" alt="Picture 2" class="header-image" />
       </a>
